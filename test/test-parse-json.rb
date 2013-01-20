ec7cbef30d42d2ca5b0e1e231b3198bf00b992b1
zunda <zunda at freeshell.org>
2013-01-19 21:10:18 -1000

    Now I can extract some properties from the JSON data

--- test/test-parse-json.rb
+++ test/test-parse-json.rb
@@ -1,7 +1,28 @@
 #!/usr/bin/ruby
-
 require 'json'
 
-# curl https://api.github.com/users/zunda/repos > zudna-list.json
-data = JSON.parse(File.open('zudna-list.json').read)
-p data
+require 'test/unit'
+
+class TestParseJson < Test::Unit::TestCase
+	def test_parse_user_repos
+		# curl https://api.github.com/users/zunda/repos > zudna-list.json
+		src_json_path = File.join(File.dirname(__FILE__), 'zudna-list.json')
+		parsed_json = JSON.parse(File.read(src_json_path))
+		repos = parsed_json.map do |element|
+			#[element['full_name'], element['git_url']]
+			element['full_name']
+		end
+		assert_equal([
+			"zunda/MogMogMonitor", "zunda/count-malloc",
+			"zunda/devquiz2011-slidepuzzle", "zunda/fluxconv", "zunda/gist",
+			"zunda/githubackup", "zunda/groonga", "zunda/hiki", "zunda/mruby",
+			"zunda/organize-photos", "zunda/rnegotter", "zunda/ruby-absolutify",
+			"zunda/ruby-mp3info", "zunda/ruby-nfork", "zunda/ruby-spiral",
+			"zunda/sorting", "zunda/sprint-android-multiple-widgets",
+			"zunda/t.co-expander", "zunda/tdiary-core",
+			"zunda/zunda-momonga-pkgs"].sort,
+			repos.sort
+		)
+	end
+
+end
