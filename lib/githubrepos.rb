5a6dfb22c2eebb3640fd536e8227daec2ca9a22c
zunda <zunda at freeshell.org>
2013-01-27 02:59:36 -1000

    GitHub::Repos.parse_json() parses JSON from github

---
+++ lib/githubrepos.rb
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2013 zunda <zunda at freeshell.org>
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+require 'json'
+
+module GitHub
+	class Repo
+		attr_reader :full_name
+		attr_reader :git_url
+
+		def initialize(full_name, git_url)
+			@full_name = full_name
+			@git_url = git_url
+		end
+	end
+
+	class Repos
+		attr_reader :entries
+
+		def initialize
+			@entries = Array.new
+		end
+
+		def parse_json(json)
+			@entries = JSON.parse(json).map{|entry|
+				Repo.new(entry['full_name'], entry['git_url'])
+			}
+		end
+
+		def Repos.parse_json(json)
+			r = Repos.new
+			r.parse_json(json)
+			r
+		end
+	end
+end
---
+++ test/testgithubrepos.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/ruby
+#
+# Copyright (C) 2013 zunda <zunda at freeshell.org>
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+require 'test/unit'
+
+$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'githubrepos'
+
+class TestRepos < Test::Unit::TestCase
+	def setup
+		@data_dir = File.dirname(__FILE__)
+	end
+
+	def test_parse_user_repos
+		src_json_path = File.join(@data_dir, 'zudna-list.json')
+		repos = GitHub::Repos.parse_json(File.read(src_json_path))
+		assert_equal([
+			"zunda/MogMogMonitor", "zunda/count-malloc",
+			"zunda/devquiz2011-slidepuzzle", "zunda/fluxconv", "zunda/gist",
+			"zunda/githubackup", "zunda/groonga", "zunda/hiki", "zunda/mruby",
+			"zunda/organize-photos", "zunda/rnegotter", "zunda/ruby-absolutify",
+			"zunda/ruby-mp3info", "zunda/ruby-nfork", "zunda/ruby-spiral",
+			"zunda/sorting", "zunda/sprint-android-multiple-widgets",
+			"zunda/t.co-expander", "zunda/tdiary-core",
+			"zunda/zunda-momonga-pkgs"].\
+			map{|e| [e, "git://github.com/#{e}.git"]}.sort,
+			repos.entries.map{|e| [e.full_name, e.git_url]}.sort
+		)
+	end
+
+	def test_parse_org_repos
+		src_json_path = File.join(@data_dir, 'edamame-list.json')
+		repos = GitHub::Repos.parse_json(File.read(src_json_path))
+		assert_equal(["EdamameTech/SiestaWatch"].\
+			map{|e| [e, "git://github.com/#{e}.git"]}.sort,
+			repos.entries.map{|e| [e.full_name, e.git_url]}.sort
+		)
+	end
+end
