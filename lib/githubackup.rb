935f7578f1f9ad6b97cf76bda5737137ae8940f5
zunda <zunda at freeshell.org>
2013-01-29 03:32:28 -1000

    User interface

---
+++ lib/githubackup.rb
@@ -0,0 +1,120 @@
+#
+# Copyright (C) 2013 zunda <zunda at freeshell.org>
+#
+# This program is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+require 'open-uri'
+require 'optparse'
+
+require 'githubackup/repos'
+require 'githubackup/copy'
+require 'githubackup/endpoints'
+
+module GitHuBackUp
+	VERSION = '0.0.1'
+	
+	def GitHuBackUp.execute(*args)
+		repos = Array.new
+		dstdir = nil
+		verbosity = 0
+		really_execute = true
+
+		op = OptionParser.new do |opts|
+			opts.banner = "usaage: #{File.basename($0, '.rb')} [options] - backs up GitHub repositories"
+
+			opts.on('-d', '--destination DIRECTORY', 'root directory of backup') do |directory|
+				dstdir = directory
+				puts "Destination directory is set to #{dstdir}" if verbosity > 0
+			end
+
+			opts.on('-u', '--user USER', 'repositories for GitHub user') do |user|
+				puts "Fetching list of repository for #{user}" if verbosity > 0
+				r = GitHub::Repos.parse_json(open(GitHubApi.user_repos(user)).read)
+				repos += r
+				if verbosity > 1
+					r.each do |repo|
+						puts "Added repository #{repo.full_name}"
+					end
+				end
+			end
+			opts.on('-o', '--org ORGANIZATION', 'repositories for GitHub organization') do |org|
+				puts "Fetching list of repository for #{org}" if verbosity > 0
+				r = GitHub::Repos.parse_json(open(GitHubApi.org_repos(org)).read)
+				repos += r
+				if verbosity > 1
+					r.each do |repo|
+						puts "Added repository #{repo.full_name}"
+					end
+				end
+			end
+			opts.on('-r', '--repository FULL_NAME', 'full name of GitHub repository') do |name|
+				puts "Fetching information for repository #{name}" if verbosity > 0
+				r = GitHub::Repo.parse_json(open(GitHubApi.full_name_repo(name)).read)
+				repos += [r]
+				if verbosity > 1
+					puts "Added repository #{r.full_name}"
+				end
+			end
+
+			opts.on('-n', '--dryrun', 'avoid actual update') do
+				really_execute = false
+			end
+
+			opts.on('-v', '--verbose', 'increase verbosity') do
+				verbosity += 1
+			end
+
+			opts.on_tail('-V', '--version') do
+				puts GitHuBackUp::VERSION
+				exit
+			end
+			opts.on_tail('-h', '--help') do
+				puts opts
+				exit
+			end
+		end
+
+		begin
+			op.parse!(args)
+			if not dstdir
+				raise OptionParser::ParseError, "Destination directory must be specified"
+			end
+			if repos.size < 1
+				raise OptionParser::ParseError, "At least one repository must be specified"
+			end
+		rescue OptionParser::ParseError => e
+			warn e
+			$stderr.puts op
+			exit
+		end
+
+		errors = false
+		repos.each do |repo|
+			if verbosity > 0
+				if really_execute
+					puts "Updating repository #{repo.full_name}"
+				else
+					puts "Pretending to update repository #{repo.full_name}"
+				end
+			end
+			begin
+				cmd = GitCopy.update_cmd(repo.full_name, repo.git_url, dstdir)
+				if really_execute
+					if verbosity < 2
+						# hide output from commands
+						cmd = "{ #{cmd}; } > /dev/null 2>&1"
+					end
+					system(cmd)
+				end
+			rescue GitCopyError => e
+				$stderr.puts e
+				errors = true
+			end
+		end
+
+		exit errors ? 1 : 0
+	end
+end
